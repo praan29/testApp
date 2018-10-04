4c27bd79392d5b021fe2a8057fd42fc5be27dd01
Anil Kumar <yak@fastmail.com>
2018-10-03 19:46:02 -0700

    Added Signup to navigation

--- router.js
+++ router.js
@@ -8,13 +8,10 @@ import ficon from 'react-native-vector-icons/FontAwesome';
 
 import Home from './assets/components/Home';
 import SignUpScreen from './assets/components/SignUp';
-
+import LoginScreen from './assets/components/LoginPage';
 import DashBoard from './assets/components/DashBoard';
 
-export const SignUp = createStackNavigator({
-	
-SignUp : SignUpScreen
-});
+
 
 
 export const UserDashBoard = createStackNavigator({
@@ -54,11 +51,31 @@ export const rootNavigator = (userSession = false)=> {
 		        //gesturesEnabled: false
 		        header : null
 		    }
+		},
+		SignUp : {
+			screen: SignUpScreen,
+			navigationOptions: {
+		        //gesturesEnabled: false
+		        header : null
+		    }
 		}
+
 	},
 	{
 		headerMode: 'none',
-		mode: "modal",
 		initialRouteName : userSession ? "UserDashBoard" : "HomePage"
 	});
 }
+
+export const SignUpStack = createStackNavigator({
+	Main: {
+      screen: LoginScreen,
+    },
+	SignUp : {
+      screen: SignUpScreen
+    }
+},
+{
+    mode: 'modal',
+    headerMode: 'none',
+  });
